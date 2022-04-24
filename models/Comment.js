93b5d89e1468756b9e639c7c3142e2b22fe9586f
Jason Wesley Smith <95160092+Jasonwesleysmith@users.noreply.github.com>
2022-04-24 01:15:22 -0500

    added validation to pizza and comment models

--- models/Comment.js
+++ models/Comment.js
@@ -1,19 +1,22 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-
 const ReplySchema = new Schema(
   {
-    // set custom id to avoid confusion with parent comment's _id field
+    // set custom id to avoid confusion with parent comment _id
     replyId: {
       type: Schema.Types.ObjectId,
       default: () => new Types.ObjectId()
     },
     replyBody: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     writtenBy: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
@@ -23,21 +26,20 @@ const ReplySchema = new Schema(
   },
   {
     toJSON: {
-      virtuals: true,
       getters: true
-      
     }
   }
 );
 
-
 const CommentSchema = new Schema(
   {
     writtenBy: {
-      type: String
+      type: String,
+      required: true
     },
     commentBody: {
-      type: String
+      type: String,
+      required: true
     },
     createdAt: {
       type: Date,
@@ -59,8 +61,6 @@ const CommentSchema = new Schema(
 CommentSchema.virtual('replyCount').get(function() {
   return this.replies.length;
 });
-
-
 
 const Comment = model('Comment', CommentSchema);
 
